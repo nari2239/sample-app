a062129239c97436b919bea66a21d20e34fdfc0c
nari2239 <oryolife@gmail.com>
2020-12-04 18:11:06 +0900

    サイドバーアイコンなどの追加

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
       color="primary"
       dark
     >
-      
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>マイアドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
     </v-app-bar>
 
     <v-main>
